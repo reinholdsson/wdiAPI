cafdbadcdc44fc82311df90608c88461343c2bdb
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-04-07 13:33:36 +0200

    Reorganized code

--- R/wdiAPI.R
+++ R/wdiAPI.R
@@ -1,21 +1,48 @@
-library(XML)
+# Function to add attributes (used in get_all_attrs)
+get_attrs <- function(x) {
+    if (!is.null(x)) {
+        attrs <- data.frame(matrix(x, nrow = 1, byrow = TRUE))
+        colnames(attrs) <- names(x)
+        return(attrs)
+    } else {
+        return(NULL)
+    }
+}
+
+# Function to get all attributes as a list
+get_all_attrs <- function(x) {
+    attrs <- xmlApply(xmlRoot(x), function(x) {
+        
+        # Root attributes
+        root <- get_attrs(xmlAttrs(x))
+        nodes <- get_attrs(unlist(xmlApply(x, xmlAttrs)))
+        
+        # Return all attributes (TODO: improve!)
+        if (is.null(root)) res <- nodes
+        else if (is.null(nodes)) res <- root
+        else res <- cbind(root, nodes)
+        
+        return(res)
+    })
+    names(attrs) <- NULL
+    return(attrs)
+}
 
 # Get data from url
-get_data_from_url <- function(url) {
-    
+get_data_from_url <- function(url, stringsAsFactors = TRUE) {
     # Get XML
     f <- xmlParse(
-            paste(readLines(url, warn = FALSE), collapse="")
+        paste(readLines(url, warn = FALSE), collapse = "")
     )
     
     # Data
-    data <- xmlToDataFrame(f, stringsAsFactors = FALSE)
-    
-    # Add ids to data
-    ids <- unlist(
-        xmlApply(xmlRoot(f), xmlAttrs)
-    )
-    data$id <- ids
+    data <- xmlToDataFrame(f, stringsAsFactors = stringsAsFactors)  # slow!
+
+    # Get all attributes (e.g. <node attribute.id = "attribute"></node>
+    attrs <- do.call("rbind", get_all_attrs(f))
+
+    # Add attributes to data
+    data <- cbind(data, attrs)
     
     return(data)
 }
@@ -31,3 +58,12 @@ get_all <- function(x) {
 # get_all("topics")
 # get_all("countries")
 # get_all("indicators")
+
+get_variable <- function(x) {
+    get_data_from_url(
+        sprintf("http://api.worldbank.org/countries/all/indicators/%s?per_page=9999", x)
+    )
+}
+
+# Examples
+# get_variable("SP.POP.TOTL")
